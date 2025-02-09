ccecc440dde019c2a32914e7b735edb7c87fb35e
jaydenli <Jaydenli1024@gmail.com>
2025-02-09 16:00:02 -0500

    fixed bug

--- src/bldcController.py
+++ src/bldcController.py
@@ -9,5 +9,7 @@ board.pwm_servo_set_position(0.1, [[2, 1500]])
 time.sleep(6)
 
 while True:
-    board.pwm_servo_set_position(0.1, [[2, input("Enter the speed: ")]])
+    board.pwm_servo_set_position(0.1, [[2, int(input("Enter the speed: "))]])
     time.sleep(2)
+
+    board.pwm_servo_set_position(0.1, [[2, 1500]])
