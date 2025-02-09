496084d41bb44b5e232b526c893943b263671644
jaydenli <Jaydenli1024@gmail.com>
2025-02-09 15:57:12 -0500

    bldc controller code

---
+++ src/bldcController.py
@@ -0,0 +1,13 @@
+import time
+import ros_robot_controller_sdk as rrc
+
+
+board = rrc.Board()
+
+# arm the ESC
+board.pwm_servo_set_position(0.1, [[2, 1500]])
+time.sleep(6)
+
+while True:
+    board.pwm_servo_set_position(0.1, [[2, input("Enter the speed: ")]])
+    time.sleep(2)
